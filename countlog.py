046e9b577be6f14b2f54abc0429ea6705fbb33fa
mpallav <mpallav@gmail.com>
2018-03-31 08:34:10 +0000

    akshaya work

---
+++ countlog.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+import re
+import datetime
+from datetime import timedelta
+#from pytz import timezone
+#import pytz
+import argparse
+
+def countStr(filename,str_match):
+    try:
+       pattern='.*'+str_match+'.*'
+ 
+       if pattern is None:
+          raise Exception('Input String match is none')
+
+       pattern = re.compile(pattern)
+
+       data= pattern.findall(filename)
+    except (IOError,EOFError) as e:
+       print 'multiple exception found' '''.format(e.args[-1])'''
+    else:
+       
+       return len(data),data   
+
+def timeElapsed(filename):
+    ts_elapse = []
+    ts_collect = []
+
+    try:
+        '''get the timestamp'''
+        for timestamp in filename:
+            ts = re.search('^\w{3}\s\d{2}\s\d{2}\:\d{2}\:\d{2}',timestamp)
+            if ts is not None:
+                ts_collect.append(ts.group())
+            else:
+                raise Exception ('regex search string is Null')
+
+        '''get the timedelta'''
+        for i in range(len(ts_collect) - 1):
+       
+            if i > 0:
+                time2 = datetime.datetime.strptime(ts_collect[i], "%b %d %H:%M:%S")
+                time1 = datetime.datetime.strptime(ts_collect[i-1], "%b %d %H:%M:%S")
+                ts_elapse.append(time2 - time1)
+           
+    except ValueError as e:
+        print 'value error found'+str(e)
+   
+    else:
+
+        avg = sum(ts_elapse,datetime.timedelta(0)) / len(ts_collect) 
+        return avg
+
+
+def utcTOpstconvertor(filename):
+    newlog = []
+    '''get the timestamp'''
+#    pst = timezone('US/Pacific')
+    for timestamp in filename:
+         ts = re.search('(^\w{3}\s\d{2}\s\d{2}\:\d{2}\:\d{2}).+(\d{2}\/\w{3}\/\d{4}\:\d{2}:\d{2}:\d{2})',timestamp)
+ 
+         timeformat1 = datetime.datetime.strptime(ts.group(1), "%b %d %H:%M:%S")
+         dt_pst1 = timeformat1.astimezone(Local)
+         new_timezone1 = dt_pst1.strptime(dt_pst1, "%b %d %H:%M:%S")
+
+         timeformat2 = datetime.datetime.strptime(ts.group(2), "%d/%b/%Y:%H:%M:%S")
+         dt_pst2 = timeformat2.astimezone(Local)
+         new_timezone2 = dt_pst2.strptime(dt_pst2, "%d/%b/%Y:%H:%M:%S")
+    
+#         replace = str(new_timezone1)+' 
+         ts = re.sub('(^\w{3}\s\d{2}\s\d{2}\:\d{2}\:\d{2}).+(\d{2}\/\w{3}\/\d{4}\:\d{2}:\d{2}:\d{2})',(str(new_timezone1))+'.+'+(str(new_timezone2)),timestamp)
+         newlog.append(ts)
+
+    return newlog
+      
+if __name__ == '__main__':
+
+#    parser = argparse.ArgumentParser(description="prase argument filename and string to search occurances")
+#    parser.add_argument('file', default='frontend-service.log',help='Enter the filename whose log needs to search')
+#    parser.add_argument('name', help='Enter the name of the string that needs to search',type=str)
+
+#    args = parser.parse_args()
+    
+     
+#    fd = open(args.file,'r')
+    fd = open('frontend-service.log','r')
+    output=fd.read()
+    
+    name = raw_input('Enter the name string to search in log file: ')
+
+    number,data =  countStr(output,name)
+
+
+    print 'Occurance in countstr'
+    print number
+#    print data 
+   
+    collect_ts = timeElapsed(data)
+    print "Average time elapsed"
+    print collect_ts
+ 
+    print_log = utcTOpstconvertor(data)
+    print print_log
